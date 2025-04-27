574e8af1106d0a485ed395e586b61abe23ab85fd
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-04-27 21:25:05 +0800

    Add new Date

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,15 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("Were currently open");
+  // else "sorry we are close";
+  // console.log(hour);
   return (
     <footer> {new Date().toLocaleTimeString()}We're currently Open</footer>
   );
